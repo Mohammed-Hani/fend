d48fb2d87dd078cdab6c8bf359bea685da76598e
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2020-10-21 19:59:20 +0200

    Update app.js
    feat: Build Nav bar dynamically
    feat: Scroll to section on Nav bar item click
    feat: Set the style of the current active section & active Nav bar item

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,13 +17,20 @@
  * Define Global Variables
  * 
 */
-
+const SECTIONS = document.querySelectorAll('section');
+const NAV_BAR_LIST = document.querySelector('#navbar__list');
+const ACTIVE_SECTION_CLASS_NAME = 'your-active-class';
+const ACTIVE_LINK_CLASS_NAME = 'active';
+const NAV_LINK_CLASS_NAME = 'menu__link';
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+
+
 
 
 
@@ -34,13 +41,49 @@
 */
 
 // build the nav
+function buildNavBar() {
+    const DOC_FRAG = document.createDocumentFragment();
+    for (let section of SECTIONS) {
+        let newNavItem = document.createElement('li');
+        let navLink = document.createElement('a');
+        navLink.classList.add(NAV_LINK_CLASS_NAME);
+        navLink.innerText = section.dataset.nav;
+        newNavItem.appendChild(navLink);
 
+        DOC_FRAG.appendChild(newNavItem);
+    }
+    NAV_BAR_LIST.appendChild(DOC_FRAG);
+
+}
 
 // Add class 'active' to section when near top of viewport
-
+function setActiveSection() {
+    const TOP_ARR = [];
+    SECTIONS.forEach(function (section) {
+        TOP_ARR.push(Math.abs(section.getBoundingClientRect().top));
+    });
+    const NEAREST_TOP_SECTION_IDX = TOP_ARR.indexOf(Math.min(...TOP_ARR));
+    if (TOP_ARR[NEAREST_TOP_SECTION_IDX] < window.innerHeight) {
+        const ACTIVE_SECTION = document.getElementById('section' + (NEAREST_TOP_SECTION_IDX + 1));
+        if (!ACTIVE_SECTION.classList.contains(ACTIVE_SECTION_CLASS_NAME)) {
+            if (document.getElementsByClassName(ACTIVE_SECTION_CLASS_NAME).length > 0) {
+                document.querySelector('.' + ACTIVE_SECTION_CLASS_NAME).classList.remove(ACTIVE_SECTION_CLASS_NAME);
+                document.querySelector('.' + ACTIVE_LINK_CLASS_NAME).classList.remove(ACTIVE_LINK_CLASS_NAME);
+            }
+            
+            ACTIVE_SECTION.classList.add(ACTIVE_SECTION_CLASS_NAME);
+            NAV_BAR_ITEMS[NEAREST_TOP_SECTION_IDX].classList.add(ACTIVE_LINK_CLASS_NAME);
+        }
+    }
+    
+    
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSection(evt) {
+    const SECTION_NAV = evt.target.innerText;
+    document.getElementById('section' + SECTION_NAV.charAt(SECTION_NAV.length - 1)).scrollIntoView({ 'behavior': 'smooth' });
+}
 
 /**
  * End Main Functions
@@ -48,10 +91,14 @@
  * 
 */
 
+
 // Build menu 
+buildNavBar();
+const NAV_BAR_ITEMS = document.querySelectorAll('.' + NAV_LINK_CLASS_NAME); 
 
 // Scroll to section on link click
+NAV_BAR_LIST.addEventListener('click', scrollToSection);
 
 // Set sections as active
-
+document.addEventListener('scroll', setActiveSection);
 
